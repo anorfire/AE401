25d0c59d28446f90fd6e34ad3d90f4261e4bbe23
anorfire <81218733+anorfire@users.noreply.github.com>
2021-11-29 15:54:33 +0800

    Update auto_Sky_garden.py

--- 11-27/auto_Sky_garden.py
+++ 11-27/auto_Sky_garden.py
@@ -1,19 +1,19 @@
 from mcpi.minecraft import Minecraft
 import random,time
-block_type=0
-mc = Minecraft.create("anorfire.club")
-uuid=mc.getPlayerEntityId("Aphrodite32728")
-x,y,z=mc.entity.getTilePos(uuid)
+block_type=0                                        #初始化方塊Type，用於判斷ID:38的花朵
+mc = Minecraft.create("anorfire.club")              #Minecraft.create與老師的伺服器建立連線
+uuid=mc.getPlayerEntityId("Aphrodite32728")         #獲取玩家的ID並存到變數UUID
+x,y,z=mc.entity.getTilePos(uuid)                    #獲取玩家座標並存到變數x、y、z
+ 
+for i in range(0,20):                               #設定條件式迴圈(這裡會輸出數字0到19)
+    mc.setBlocks(x,y-1+i,z+i,x+2,y-1+i,z+i,155)     #mc.setblocks(x座標,y座標-1+迴圈數字i,z+迴圈數字i,x+2,y-1+迴圈數字i,z+迴圈數字i,方塊id)
+    time.sleep(0.5)                                 #使程式休息一下
+    mc.entity.setTilePos(uuid,x+1,y+i,z+i)          #設定玩家座標使玩家能夠自動上樓梯 mc.entity.setTilePos(玩家uuid,座標)
 
-for i in range(0,20):
-    mc.setBlocks(x,y-1+i,z+i,x+2,y-1+i,z+i,155)
-    time.sleep(0.5)
-    mc.entity.setTilePos(uuid,x+1,y+i,z+i)
-
-x,y,z=mc.entity.getTilePos(uuid)
-mc.setBlocks(x-3,y-1,z,x+3,y-1,z+6,2)
-blockid=random.choice([37,38])
-if blockid==38:
+x,y,z=mc.entity.getTilePos(uuid)                    #重新獲取玩家上完樓梯後的座標x、y、z
+mc.setBlocks(x-3,y-1,z,x+3,y-1,z+6,2)               #創造花園的平台
+blockid=random.choice([37,38])                      #隨機選擇ID:37或38的花朵
+if blockid==38:                                     #如果隨機選到38的花朵，在選一次38的type
     block_type=random.randint(0,8)
 
-mc.setBlocks(x-3,y,z,x+3,y,z+6,blockid,block_type)
+mc.setBlocks(x-3,y,z,x+3,y,z+6,blockid,block_type)  #將上面選擇出來的花放在平台上
