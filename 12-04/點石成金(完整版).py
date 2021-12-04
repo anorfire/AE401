d5fa35492d174f558b9bebd0092211ea5ff5330d
anorfire <81218733+anorfire@users.noreply.github.com>
2021-12-04 20:29:05 +0800

    Update 點石成金(完整版).py

--- 12-04/點石成金(完整版).py
+++ 12-04/點石成金(完整版).py
@@ -2,10 +2,10 @@ from mcpi.minecraft import Minecraft
 mc=Minecraft.create()
 
 while True:
-    hits = mc.events.pollBlockHits()
-    if len(hits)>0:
-        hit = hits[0]
-        x,y,z = hit.pos.x,hit.pos.y,hit.pos.z
-        block=mc.getBlock(x,y,z)
-        mc.setBlock(x,y,z,41)
+    hits = mc.events.pollBlockHits()                                #偵測使用劍點左鍵
+    if len(hits)>0:                                                 #判斷是否取得到hits
+        hit = hits[0]                                               #將取得道的hit[0]放進hit
+        x, y, z = hit.pos.x, hit.pos.y, hit.pos.z                   #獲得x,y,z位置
+        block=mc.getBlock(x,y,z)                                    #獲得block ID
+        mc.setBlock(x,y,z,41)                                       #將獲取的位置改變成金磚
         mc.postToChat("將原本方塊ID:" + str(block) + "變成金磚!")
