ffedc3dae1bcac8b7b7f05fc4a73380e6d02d2b7
anorfire <81218733+anorfire@users.noreply.github.com>
2021-12-04 20:32:12 +0800

    Update 召喚小雞.py

--- 12-04/召喚小雞.py
+++ 12-04/召喚小雞.py
@@ -5,4 +5,4 @@ uuid=mc.getPlayerEntityId("Aphrodite32728")
 x, y, z=mc.entity.getTilePos(uuid)
 
 for i in range(5):                              #使用條件迴圈執行五次召喚小雞
-    mc.spawnEntity(x,y,z,93)                    #小雞ID:93
+    mc.spawnEntity(x, y, z,93)                  #小雞ID:93
