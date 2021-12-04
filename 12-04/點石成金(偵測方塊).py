e034db18fa1f1778c625613248a939bea963ff8f
anorfire <81218733+anorfire@users.noreply.github.com>
2021-12-04 20:30:24 +0800

    Update 點石成金(偵測方塊).py

--- 12-04/點石成金(偵測方塊).py
+++ 12-04/點石成金(偵測方塊).py
@@ -2,9 +2,9 @@ from mcpi.minecraft import Minecraft
 mc=Minecraft.create()
 
 while True:
-    hits = mc.events.pollBlockHits()
-    if len(hits)>0:
-        hit = hits[0]
-        x,y,z = hit.pos.x,hit.pos.y,hit.pos.z
-        block=mc.getBlock(x,y,z)
+    hits = mc.events.pollBlockHits()                #偵測使用劍點左鍵 
+    if len(hits)>0:                                 #判斷是否取得到hits
+        hit = hits[0]                               #將取得道的hit[0]放進hit
+        x,y,z = hit.pos.x,hit.pos.y,hit.pos.z       #獲得x,y,z位置
+        block=mc.getBlock(x,y,z)                    #獲取方塊ID
         mc.postToChat("獵到的方塊:"+str(block))
