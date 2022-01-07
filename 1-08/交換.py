e9b7c47307986023da3fc1875f4bdf4cb50ba800
anorfire <81218733+anorfire@users.noreply.github.com>
2022-01-08 04:56:04 +0800

    Create 交換.py

---
+++ 1-08/交換.py
@@ -0,0 +1,9 @@
+A=67
+B=35
+#-------------------#
+T=A
+print("第一輪交換 A="+str(A)," B="+str(B), " 這時候的T=",str(T))
+A=B
+print("第二輪交換 A="+str(A)," B="+str(B), " 這時候的T=",str(T))
+B=T
+print("第三輪交換 A="+str(A)," B="+str(B), " 這時候的T=",str(T))
